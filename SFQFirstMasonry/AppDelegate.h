80f0f500937c99ddf8b6b2f83713ed3027df281b
bestsonqi@163.com <bestsonqi@163.com>
2016-05-09 11:41:28 +0800

    Initial Commit

---
+++ SFQFirstMasonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFQFirstMasonry
+//
+//  Created by APPLE on 16/5/9.
+//  Copyright © 2016年 songfangqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
